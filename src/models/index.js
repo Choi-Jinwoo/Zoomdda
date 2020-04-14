3f2af4ffb69d4ea5168e30fb5cb0d5ac76e9d1d7
Choi-Jinwoo <49791336+Choi-Jinwoo@users.noreply.github.com>
2020-04-14 22:31:33 +0900

    [FIX] Model Sync error

--- src/models/index.js
+++ src/models/index.js
@@ -1,8 +1,10 @@
+const fs = require('fs');
+const path = require('path');
 const Sequelize = require('sequelize');
 
 const { mysql: config } = require('../../config/database.json');
 
-const TimeTable = require('./TimeTable');
+const models = {};
 
 const sequelize = new Sequelize(
   config.database,
@@ -12,10 +14,26 @@ const sequelize = new Sequelize(
     host: config.host,
     dialect: config.dialect,
     port: config.port,
+    logging: false,
   },
 );
 
-sequelize.TimeTable = TimeTable;
+
+fs.readdirSync(__dirname)
+  .filter((file) => (file.indexOf('.') !== 0) && (file !== 'index.js') && (!fs.statSync(path.join(__dirname, file)).isDirectory()))
+  .forEach((file) => {
+    const extName = path.extname(path.join(__dirname, file));
+    const baseName = path.basename(path.join(__dirname, file), extName);
+
+    const model = sequelize.import(path.join(__dirname, file));
+    models[baseName] = model;
+  });
+
+Object.keys(models).forEach((modelName) => {
+  if ('associate' in models[modelName]) {
+    models[modelName].associate(models);
+  }
+});
 
 sequelize.sync({ force: false }).then(() => {
   console.log('[MODEL - SYNC] DB 연결')
@@ -23,8 +41,7 @@ sequelize.sync({ force: false }).then(() => {
   console.log('[MODEL - SYNC] DB 연결 오류')
 });
 
-const models = {
-  TimeTable,
-  sequelize,
-  Sequelize,
-};
+models.sequelize = sequelize;
+models.Sequelize = Sequelize;
+
+module.exports = models;
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors')
 const bodyParser = require('body-parser')
+const models = require('./models');
 
 const app = express();
 
